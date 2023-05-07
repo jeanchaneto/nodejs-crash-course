81cb0d12ab61fd8c79e4d6539952f75b1b7721c4
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-05-07 20:47:42 +0200

    Basic routing with express

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
@@ -6,7 +6,14 @@ const app = express();
 app.listen(3000);
 
 //Respond to request
-app.get('/', (req, res) => {
-    //Response automatically sets the header and status code
-    res.send('<p>EXPRESS</p>')
-})
+app.get("/", (req, res) => {
+  //Response automatically sets the header and status code
+  // res.send('<p>EXPRESS</p>')
+  res.sendFile("/views/index.html", { root: __dirname });
+});
+
+app.get("/about", (req, res) => {
+  //Response automatically sets the header and status code
+  //looks for absolute path so specify root as 2nd argument
+  res.sendFile("./views/about.html", { root: __dirname });
+});
