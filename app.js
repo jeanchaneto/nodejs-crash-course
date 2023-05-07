9d1c72ca1283ac2a336faacb6d7c673da05e4592
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-05-07 21:50:55 +0200

    Passing data to views

--- app.js
+++ app.js
@@ -2,20 +2,43 @@ const express = require("express");
 
 const app = express();
 
+//Register view engine
+app.set("view engine", "ejs");
+//if views in differetn folder than views:
+//app.set('views', 'CUSTOM_VIEW_FOLDER_NAME')
+
 //Listen for request
 app.listen(3000);
 
 //Respond to request
 app.get("/", (req, res) => {
   //Response automatically sets the header and status code
+
   // res.send('<p>EXPRESS</p>')
-  res.sendFile("/views/index.html", { root: __dirname });
+  //   res.sendFile("/views/index.html", { root: __dirname });
+
+  const blogs = [
+    { title: "zefez", content: "fzefezfbcuiBUIBCBEbf" },
+    { title: "ver", content: "czenceznonec fnzi" },
+    { title: "ztg", content: "jvniuqbnvuqn  oifnezinf zefz" },
+    { title: "pok", content: "fqf,io EFNIOZnef ion" },
+  ];
+
+  // View engine response
+  res.render("index", { message: "Dynamic ejs message", blogs: blogs });
 });
 
 app.get("/about", (req, res) => {
   //Response automatically sets the header and status code
+
   //looks for absolute path so specify root as 2nd argument
-  res.sendFile("./views/about.html", { root: __dirname });
+  //   res.sendFile("./views/about.html", { root: __dirname });
+
+  res.render("about");
+});
+
+app.get("/blog/create", (req, res) => {
+  res.render("create");
 });
 
 //redirect
@@ -25,6 +48,7 @@ app.get("/about-me", (req, res) => {
 
 //404 located last as express reads top to bottom
 app.use((req, res) => {
-    res.status(404).sendFile("./views/404.html", { root: __dirname });
-}
-);
+  //   res.status(404).sendFile("./views/404.html", { root: __dirname });
+
+  res.render("404");
+});
