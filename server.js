12cf92d90da1d88b2d3bc241e3a9f40d51a0fae7
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-05-07 18:22:57 +0200

    Basic routing

--- server.js
+++ server.js
@@ -1,33 +1,44 @@
 const http = require("http");
-const fs = require('fs');
+const fs = require("fs");
 
 //Create server
 const server = http.createServer((req, res) => {
-  
-    console.log(req.url, req.method);
+  console.log(req.url, req.method);
+
+  //Set routing path
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      break;
+    case "/about":
+      path += "about.html";
+      break;
+    default:
+      path += "404.html";
+      break;
+  }
 
   //Set header content type
   res.setHeader("content-type", "text/html");
 
-//   //Write response
-//   res.write("<h1>Response Title</h1>");
-//   res.write("<p>HTML response</p>");
-//   //Send response to brower
-//   res.end();
+  //   //Write response
+  //   res.write("<h1>Response Title</h1>");
+  //   res.write("<p>HTML response</p>");
+  //   //Send response to brower
+  //   res.end();
 
-//send an html file as response
-fs.readFile('./views/index.html', (err, data) => {
-    if(err) {
-        console.log(err);
-        res.end()
+  //send an html file as response
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
     } else {
-        // res.write(data);
-        // If used once can put data in res.end()
-        res.end(data)
+      // res.write(data);
+      // If used once can put data in res.end()
+      res.end(data);
     }
-})
-
-
+  });
 });
 
 //Listen to http requests
