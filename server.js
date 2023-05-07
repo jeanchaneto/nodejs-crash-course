44b04663e534124317a340901014d496ceb1ca8c
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-05-07 18:10:13 +0200

    Send Html file as response to browser

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const fs = require('fs');
 
 //Create server
 const server = http.createServer((req, res) => {
@@ -7,11 +8,26 @@ const server = http.createServer((req, res) => {
 
   //Set header content type
   res.setHeader("content-type", "text/html");
-  //Write response
-  res.write("<h1>Response Title</h1>");
-  res.write("<p>HTML response</p>");
-  //Send response to brower
-  res.end();
+
+//   //Write response
+//   res.write("<h1>Response Title</h1>");
+//   res.write("<p>HTML response</p>");
+//   //Send response to brower
+//   res.end();
+
+//send an html file as response
+fs.readFile('./views/index.html', (err, data) => {
+    if(err) {
+        console.log(err);
+        res.end()
+    } else {
+        // res.write(data);
+        // If used once can put data in res.end()
+        res.end(data)
+    }
+})
+
+
 });
 
 //Listen to http requests
